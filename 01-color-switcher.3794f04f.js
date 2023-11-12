5a2572ee7e5f426db8fdb80d10022837fe4a3024
Cris8791 <135622871+Cris8791@users.noreply.github.com>
2023-11-12 15:38:10 +0000

    Deploying to gh-pages from @ Cris8791/goit-js-hw-09@e89f1f9206f7ac4d38419d654671034de3e48a30 🚀

---
+++ 01-color-switcher.3794f04f.js
@@ -0,0 +1,10 @@
+let t;// adaug evenimente pentru butoane
+document.querySelector("[data-start]").addEventListener("click",function(){// dezactivez butonul "Start"
+document.querySelector("[data-start]").disabled=!0,// activez butonul "Stop"
+document.querySelector("[data-stop]").disabled=!1,// Schimbă culoarea de fundal o dată pe secundă
+t=setInterval(()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`},1e3)}),document.querySelector("[data-stop]").addEventListener("click",function(){// activez butonul "Start"
+document.querySelector("[data-start]").disabled=!1,// dezactivez butonul "Stop"
+document.querySelector("[data-stop]").disabled=!0,// opresc schimbarea culorii de fundal
+clearInterval(t)});//# sourceMappingURL=01-color-switcher.3794f04f.js.map
+
+//# sourceMappingURL=01-color-switcher.3794f04f.js.map
---
+++ 03-promises.5832083f.js
@@ -0,0 +1,17 @@
+!function(){function e(e){return e&&e.__esModule?e.default:e}var t="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},o={},i=t.parcelRequire7bc7;null==i&&((i=function(e){if(e in n)return n[e].exports;if(e in o){var t=o[e];delete o[e];var i={id:e,exports:{}};return n[e]=i,t.call(i.exports,i,i.exports),i.exports}var r=Error("Cannot find module '"+e+"'");throw r.code="MODULE_NOT_FOUND",r}).register=function(e,t){o[e]=t},t.parcelRequire7bc7=i),i.register;// Cerinta o regasesti in fisierul cerinte_teme
+// Notiflix este o librarie folosita pt funcționalitatile de notificare
+var r=i("6JpON");// adaugare handler pt evenimentul DOMContentLoaded, care este declansat
+// cand documentul HTML a fost complet incarcat si parsat.
+document.addEventListener("DOMContentLoaded",function(){// adaugare handler pt evenimentul "submit" al formularului.
+let t=document.querySelector(".form");t.addEventListener("submit",function(n){n.preventDefault();// extragere valori introduse in campurile formularului:
+// întârzierea(delay), pas(step) și cantitate(amount).
+let o=parseInt(t.elements.delay.value,10),i=parseInt(t.elements.step.value,10),l=parseInt(t.elements.amount.value,10);// bucla for care rulează de la 1 până la amount
+for(let t=1;t<=l;t++){let n=t,l=o+(t-1)*i;// pt fiecare promisiune, se adauga un handler then pentru rezolvare si
+// un handler catch pentru respingere si se afișează notificari
+// corespunzatoare cu ajutorul Notiflix
+// definire functie createPromise care intoarce o promisiune.
+(function(e,t){return new Promise((n,o)=>{// se decide aleatoriu daca promisiunea  ar trebui sa fie rezolvata sau respinsa;
+// promisiunea este rezolvata daca valoarea generata aleatoriu este mai mare decat 0.3, altfel este respinsă
+let i=Math.random()>.3;setTimeout(()=>{i?n({position:e,delay:t}):o({position:e,delay:t})},t)})})(n,l).then(({position:t,delay:n})=>{/*@__PURE__*/e(r).Notify.success(`\u{2705} Fulfilled promise ${t} in ${n}ms`)}).catch(({position:t,delay:n})=>{/*@__PURE__*/e(r).Notify.failure(`\u{274C} Rejected promise ${t} in ${n}ms`)})}})})}();//# sourceMappingURL=03-promises.5832083f.js.map
+
+//# sourceMappingURL=03-promises.5832083f.js.map
